0497274afead751d5427ce156e1b7d8af96c9bfc
Loren Wedderburn <lorenwedderburn@hotmail.com>
2026-03-18 05:38:57 +0000

    initial commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,28 @@
+import "../css/favorites.css";
+import { useMovieContext } from "../components/UseMovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (favorites) {
+    return (
+      <div>
+        <h2 className="favorites">Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="favorites-empty">
+      <h2>No favorite movies yet</h2>
+      <p>Start adding movies to your favorites</p>
+    </div>
+  );
+}
+
+export default Favorites;
---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "f485b8365f23901d2a43424d4bf06f84";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`,
+  );
+  const data = await response.json();
+  return data.results;
+};
